4547e762f936aa3f685db3c88fcaedf91270d9ea
Yashovardhan Shaktawat <yashovardhanshaktawat@gmail.com>
2020-10-29 09:52:09 +0530

    CleanUp

---
+++ app/src/main/java/com/ibm/androidapp/CleanUp.java
@@ -0,0 +1,14 @@
+package com.ibm.androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CleanUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clean_up);
+    }
+}
